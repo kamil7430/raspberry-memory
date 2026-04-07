9860b74595facab56947d0e5bbbea1c5471dd180
Kamil Błażejczyk <01189072@pw.edu.pl>
2026-04-07 22:38:30 +0200

    Add game initialization

--- blink.c
+++ blink.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <unistd.h>
-#include <periphery/gpio.h>
+// #include <periphery/gpio.h>
 
-// #include "includes/gpio.h"
+#include "includes/gpio.h"
 
 int main(void) {
     const int DIODES = 4;
@@ -21,12 +20,17 @@ int main(void) {
 
     const char *FILE_PATH = "/dev/gpiochip0";
 
+    const int GAME_LENGTH = 5;
+    const int SECONDS_TO_MEMORIZE = 1;
+
     gpio_t *gpios[] = {
         // diodes
         gpio_new(), gpio_new(), gpio_new(), gpio_new(), 
         // buttons
         gpio_new(), gpio_new(), gpio_new(), gpio_new(), 
     };
+
+    puts("Initializing the game...");
 
     // Open gpios
     for (int i = 0; i < DIODES; i++) {
@@ -50,11 +54,37 @@ int main(void) {
         }
     }
 
-    // Main game loop
+    // Initialize game
+    int sequence[GAME_LENGTH];
+    for (int i = 0; i < GAME_LENGTH; i++) {
+        sequence[i] = rand() % BUTTONS;
+    }
+
+    puts("Sequence showing phase starts!");
+
+    // Sequence showing phase
+    for (int i = 0; i < GAME_LENGTH; i++) {
+        if (gpio_write(gpios[sequence[i]], true) < 0) {
+            fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(gpios[sequence[i]]));
+            exit(1);
+        }
+        sleep(SECONDS_TO_MEMORIZE);
+        if (gpio_write(gpios[sequence[i]], false) < 0) {
+            fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(gpios[sequence[i]]));
+            exit(1);
+        }
+        sleep(SECONDS_TO_MEMORIZE);
+    }
+
+    puts("Sequence guessing phase starts!");
+
+    // Sequence guessing phase
     bool poll_states[BUTTONS] = {};
-    while (true) {
-        if (gpio_poll_multiple(gpios + DIODES, BUTTONS, -1, poll_states) < 0)
+    while (1) {
+        if (gpio_poll_multiple(gpios + DIODES, BUTTONS, -1, poll_states) < 0) {
+            fprintf(stderr, "gpio_poll_multiple() error\n");
             break;
+        }
 
         // Sleep for 20ms since the kernel does not support debounce
         usleep(20000);
@@ -76,6 +106,7 @@ int main(void) {
                 printf("Kliknieto przycisk 2\n");
             }
         }
+
         if (poll_states[2]) {
             bool level;
             gpio_read(gpios[2], &level);
@@ -84,6 +115,7 @@ int main(void) {
                 printf("Kliknieto przycisk 3\n");
             }
         }
+
         if (poll_states[3]) {
             bool level;
             gpio_read(gpios[3], &level);
@@ -102,20 +134,3 @@ int main(void) {
 
     return 0;
 }
-//    for (int i = 0; i < 5; i++) {
-//        /* Write output GPIO with !value */
-//        if (gpio_write(gpio_out, true) < 0) {
-//            fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(gpio_out));
-//            exit(1);
-//        }
-//        sleep(2);
-//
-//        if (gpio_write(gpio_out, false) < 0) {
-//            fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(gpio_out));
-//            exit(1);
-//        }
-//        sleep(2);
-//    }
-//
-//    gpio_close(gpio_out);
-//    gpio_free(gpio_out);
