489e0946089e461c8ef3ceeba8478c1ad3bdd292
Kamil Błażejczyk <01189072@pw.edu.pl>
2026-04-07 22:55:05 +0200

    Probably finish the game

--- blink.c
+++ blink.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
-// #include <periphery/gpio.h>
+#include <periphery/gpio.h>
 
-#include "includes/gpio.h"
+// #include "includes/gpio.h"
 
 int main(void) {
     const int DIODES = 4;
@@ -59,6 +59,7 @@ int main(void) {
     }
 
     puts("Sequence showing phase starts!");
+    puts("Observe the diodes!");
     for (int i = 0; i < GAME_LENGTH; i++) {
         if (gpio_write(gpios[sequence[i]], true) < 0) {
             fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(gpios[sequence[i]]));
@@ -74,7 +75,9 @@ int main(void) {
 
     puts("Sequence guessing phase starts!");
     bool poll_states[BUTTONS] = {};
-    while (1) {
+    int seq_idx = 0;
+    puts("Make your first choice now!");
+    while (true) {
         if (gpio_poll_multiple(gpios + DIODES, BUTTONS, -1, poll_states) < 0) {
             fprintf(stderr, "gpio_poll_multiple() error\n");
             break;
@@ -83,39 +86,31 @@ int main(void) {
         // Sleep for 20ms since the kernel does not support debounce
         usleep(20000);
 
-        if (poll_states[0]) {
-            bool level;
-            gpio_read(gpios[0], &level);
-
-            if (!level) {
-                printf("Kliknieto przycisk 1\n");
+        int clicked_idx = -1;
+        for (int i = 0; i < BUTTONS; i++) {
+            if (poll_states[i]) {
+                bool level;
+                gpio_read(gpios[i], &level);
+                if (!level) {
+                    clicked_idx = i;
+                }
             }
         }
 
-        if (poll_states[1]) {
-            bool level;
-            gpio_read(gpios[1], &level);
-
-            if (!level) {
-                printf("Kliknieto przycisk 2\n");
-            }
-        }
-
-        if (poll_states[2]) {
-            bool level;
-            gpio_read(gpios[2], &level);
-
-            if (!level) {
-                printf("Kliknieto przycisk 3\n");
-            }
-        }
-
-        if (poll_states[3]) {
-            bool level;
-            gpio_read(gpios[3], &level);
-
-            if (!level) {
-                printf("Kliknieto przycisk 4\n");
+        if (clicked_idx >= 0) {
+            printf("Button %d clicked!\n", clicked_idx + 1);
+            if (sequence[seq_idx] == clicked_idx) {
+                puts("That is correct!");
+                seq_idx++;
+                if (seq_idx == GAME_LENGTH) {
+                    puts("Congratulations! You won!");
+                    break;
+                }
+                sleep(1);
+                printf("Make your %d choice now!", seq_idx + 1);
+            } else {
+                puts("Unfortunately, this is incorrect choice :(\nYou lost!");
+                break;
             }
         }
     }
